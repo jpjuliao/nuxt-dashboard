aaca2e3529a88c4927bd16078c127f9c145b85ea
JP Juliao <jpjuliao@gmail.com>
2024-05-06 09:37:15 -0500

    Add shadcn nav-menu to nuxt app

--- app.vue
+++ app.vue
@@ -1,5 +1,27 @@
+<script setup lang="ts">
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from '@/components/ui/navigation-menu'
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NavigationMenu>
+    <NavigationMenuList>
+      <NavigationMenuItem>
+        <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+        <NavigationMenuTrigger>Item Two</NavigationMenuTrigger>
+        <NavigationMenuTrigger>Item Three</NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <NavigationMenuLink>Link</NavigationMenuLink>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+  </NavigationMenu>
 </template>
