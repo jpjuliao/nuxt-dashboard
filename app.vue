f2e74d82c23ae7939771372319bdf3582890ec9f
JP Juliao <jpjuliao@gmail.com>
2024-05-06 09:42:35 -0500

    Add shadcn carousel and card

--- app.vue
+++ app.vue
@@ -2,13 +2,15 @@
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from '@/components/ui/navigation-menu'
+
+
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
+import { Card, CardContent } from '@/components/ui/card'
 </script>
 
 <template>
@@ -24,4 +26,20 @@ import {
       </NavigationMenuItem>
     </NavigationMenuList>
   </NavigationMenu>
+
+  <Carousel class="relative w-full max-w-xs">
+    <CarouselContent>
+      <CarouselItem v-for="(_, index) in 5" :key="index">
+        <div class="p-1">
+          <Card>
+            <CardContent class="flex aspect-square items-center justify-center p-6">
+              <span class="text-4xl font-semibold">{{ index + 1 }}</span>
+            </CardContent>
+          </Card>
+        </div>
+      </CarouselItem>
+    </CarouselContent>
+    <CarouselPrevious />
+    <CarouselNext />
+  </Carousel>
 </template>
